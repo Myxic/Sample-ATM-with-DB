b1f12a229e5ea41a14f53c4c3c20c73c81ba68ff
Ezemenahi Benedict <ezemenahi56@gmail.com>
2023-03-01 12:36:06 +0100

    upload

---
+++ DatabaseProjectATMDATABASE/dbo/Tables/BankAdmin.sql
@@ -0,0 +1,12 @@
+CREATE TABLE [dbo].[BankAdmin] (
+    [id]           INT          NULL,
+    [First_name]   VARCHAR (50) NULL,
+    [Last_name]    VARCHAR (50) NULL,
+    [Gender]       VARCHAR (50) NULL,
+    [PinCode]      INT          NULL,
+    [Phone_Number] VARCHAR (50) NULL
+);
+
+
+GO
+
